7edf438254aa66bc690da01c98b1bddaf2fb3ba6
ckangel <ckajeigbe1@gmail.com>
2025-09-07 23:51:21 +0000

    add .env and .gitiignore

--- frontend-backend-api/server.js
+++ frontend-backend-api/server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const app = express();
+require("dotenv").config();
 const PORT = process.env.PORT || 8080;
 
 // Middleware
